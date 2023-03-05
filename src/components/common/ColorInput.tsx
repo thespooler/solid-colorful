1738e08825f176c3c6f82602cc5287cd675112c3
TheSpooler <spooler@venrik.com>
2023-03-05 12:24:53 -0500

    Revet change

--- src/components/common/ColorInput.tsx
+++ src/components/common/ColorInput.tsx
@@ -1,4 +1,4 @@
-import { JSX, mergeProps, splitProps } from "solid-js";
+import { createEffect, createSignal, JSX, mergeProps, splitProps, untrack } from "solid-js";
 import { ColorInputBaseProps } from "../../types";
 
 interface Props extends ColorInputBaseProps {
@@ -13,9 +13,6 @@ interface Props extends ColorInputBaseProps {
 }
 
 export const ColorInput = (props: Props): JSX.Element => {
-  // eslint-disable-next-line prefer-const
-  let input: HTMLInputElement | undefined = undefined;
-
   const [localprops, otherprops] = splitProps(props, [
     "onBlur",
     "onChange",
@@ -25,7 +22,9 @@ export const ColorInput = (props: Props): JSX.Element => {
     "format",
     "escape",
   ]);
-
+  const [value, setValue] = createSignal(
+    untrack(() => (localprops.color ? localprops.escape(localprops.color) : undefined))
+  );
   const mergedlocalprops = mergeProps(
     {
       format: (f: string | undefined) => f ?? "",
@@ -37,16 +36,16 @@ export const ColorInput = (props: Props): JSX.Element => {
   // Trigger `onChange` handler only if the input value is a valid color
   const handleChange = (e: Event & { currentTarget: HTMLInputElement; target: Element }) => {
     const inputValue = localprops.escape(e.currentTarget.value);
-    if (localprops.validate(inputValue) && localprops.onChange !== undefined) {
-      localprops.onChange(mergedlocalprops.process(inputValue));
+    if (localprops.validate(inputValue)) {
+      setValue(inputValue);
     }
   };
 
   // Take the color from props if the last typed color (in local state) is not valid
   const handleBlur = (e: FocusEvent & { currentTarget: HTMLInputElement; target: Element }) => {
     if (!localprops.validate(e.currentTarget.value)) {
-      // Forecully reset textbox value
-      if (input !== undefined) input.value = mergedlocalprops.format(localprops.color ?? "");
+      const colorValue = localprops.color ? localprops.escape(localprops.color) : undefined;
+      setValue(colorValue);
     }
     const onBlur = localprops.onBlur as JSX.EventHandler<HTMLInputElement, FocusEvent>;
     if (onBlur !== undefined) {
@@ -54,11 +53,21 @@ export const ColorInput = (props: Props): JSX.Element => {
     }
   };
 
+  createEffect(() => {
+    setValue(localprops.color ? localprops.escape(localprops.color) : undefined);
+  });
+
+  createEffect(() => {
+    const valid_value = value();
+    if (localprops.onChange !== undefined && valid_value !== undefined) {
+      localprops.onChange(mergedlocalprops.process(valid_value));
+    }
+  });
+
   return (
     <input
       {...otherprops}
-      ref={input}
-      value={localprops.color}
+      value={mergedlocalprops.format(value())}
       spellcheck={false} // the element should not be checked for spelling errors
       onChange={handleChange}
       onBlur={handleBlur}
